48f05865f09afb200acca90509d2dfd1031c1275
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-05-07 15:48:46 -0700

    assignment complete

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,5 +1,5 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+import { Vehicle } from "./vehicleBaseClass";
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
